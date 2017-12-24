d15a5e3443bac14a65706913d0cd7587606a41e9
James N. V. Cash <james.nvc@gmail.com>
2017-12-24 12:23:23 -0500

    Make config an infotab file, move to top level

--- config.rkt
+++ config.rkt
@@ -1,6 +1,4 @@
-#lang racket/base
-
-(provide bot-id bot-token braid-url)
+#lang info
 
 (define bot-id "5a3ebd7f-eb55-46c2-b684-949e7652fcc8")
 (define bot-token "bhti_MBYHReCk66BIn63TbWD1yp7DD1Y1wfmmqC-")
