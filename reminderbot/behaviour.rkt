07369c24c365a9cff6f0bef7c9feba1b08847bc2
James N. V. Cash <james.nvc@gmail.com>
2018-01-01 14:46:42 -0500

    Allow setting text of reminder

--- reminderbot/behaviour.rkt
+++ reminderbot/behaviour.rkt
@@ -9,28 +9,28 @@
          "util.rkt"
          "braid.rkt")
 
-(define (parse-seconds str)
-  (some-> (regexp-match #rx"^.*([0-9]+) minutes$" str)
-          cadr
-          string->number
-          (* 60)))
+(define reminder-re #rx"^/reminderbot (.*) in ([0-9]+) minutes$")
+
+(define (parse-reminder str)
+  (if-let [matches (regexp-match reminder-re str)]
+    (let ([secs (-> matches caddr string->number (* 60))]
+          [txt (cadr matches)])
+      (list secs txt))
+    #f))
 
 (define (act-on-message msg)
-  (->> (if-let [secs (parse-seconds (hash-ref msg '#:content))]
-        (begin
+  (->> (if-let [rem (parse-reminder (hash-ref msg '#:content))]
+        (let ([secs (car rem)]
+              [txt (cadr rem)])
           (thread
            (λ ()
              (with-task-server
-              (delayed-task
-               secs
-               (->> (list "This is your reminder you requested "
-                         (number->string secs) "seconds ago")
-                   (string-join)
-                   (reply-to msg)))
+              (delayed-task secs (reply-to msg txt))
               (run-tasks))))
           (-> (list "Okay, I'll remind you in" (number->string secs) "seconds")
               string-join))
         (-> (list "Sorry, I don't know what that means."
-                  "Try something like `/reminderbot 5 minutes`.")
+                  "Try something like "
+                  "`/reminderbot check laundry in 5 minutes`.")
             (string-join "\n")))
       (reply-to msg)))
