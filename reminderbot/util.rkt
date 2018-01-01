f72de68a788a72a87249cfc91cc6887df15b59c8
James N. V. Cash <james.nvc@gmail.com>
2018-01-01 14:28:26 -0500

    Add some more helper macros

--- reminderbot/util.rkt
+++ reminderbot/util.rkt
@@ -3,7 +3,9 @@
 (provide
  ->
  ->>
- some->>)
+ some->
+ some->>
+ if-let)
 
 (define-syntax ->
   (syntax-rules ()
@@ -18,8 +20,23 @@
     [(_ x f) (f x)]
     [(_ x f g ...) (->> (->> x f) g ...)]))
 
+(define-syntax some->
+  (syntax-rules ()
+    [(_ x) x]
+    [(_ x (f a ...)) (if x (f x a ...) x)]
+    [(_ x f) (if x (f x) x)]
+    [(_ x f g ...) (if x
+                      (some-> (some-> x f) g ...)
+                      x)]))
+
 (define-syntax some->>
   (syntax-rules ()
     [(_ x (f a ...)) (if x (f a ... x) x)]
     [(_ x f) (if x (f x) x)]
     [(_ x f g ...) (if x (some->> (some->> x f) g ...) x)]))
+
+(define-syntax if-let
+  (syntax-rules ()
+    [(_ (var val) t e)
+     (let ([var val])
+       (if var t e))]))
