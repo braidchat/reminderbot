02a8468a403bd4c6f776296af6c82877e06d4e80
James N. V. Cash <james.nvc@gmail.com>
2018-01-05 14:19:35 -0500

    Depend on github tag for braidbot package

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "reminderbot")
 (define deps '("base"
-               "braidbot"
+               "https://github.com/braidchat/braidbot.git#v1.0"
                "tasks"))
 (define build-deps '("web-server-lib"))
 (define pkg-desc "Description Here")
