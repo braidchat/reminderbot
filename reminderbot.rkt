9e65ad1ed1566226832bcb559496be17c7ed247d
James N. V. Cash <james.nvc@gmail.com>
2018-01-05 14:04:37 -0500

    Explicitly set port

--- reminderbot.rkt
+++ reminderbot.rkt
@@ -5,12 +5,13 @@
          tasks
 
          braidbot/util
-         braidbot/uuid
-         braidbot/braid)
+         braidbot/uuid)
 
 (define bot-id "5a47c63e-8d23-4bba-92ef-53c3c0f7e398")
 (define bot-token "4S3EzYM-exfyBibpYxEggiX7ahcWrRh3AIKoKbms")
 (define braid-url "http://localhost:5557")
+
+(listen-port 8899)
 
 (define reminder-re #rx"^/reminderbot (.*) in ([0-9]+) minutes?$")
 
